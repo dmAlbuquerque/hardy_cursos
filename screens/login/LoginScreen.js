c5a61753cf9b74cdcb14688e463d327686435292
dmAlbuquerque <dm.albuquerque.dev@gmail.com>
2020-01-31 12:42:51 -0300

    first commit

---
+++ screens/login/LoginScreen.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+import { View } from 'react-native';
+
+// import { Container } from './styles';
+
+export default LoginScreen(){
+    return (
+        <View>
+        </View>
+    );
+
+    const styles = StyleSheet.create({
+        container: {
+            flex: 1,
+            backgroundColor: '#fff',
+        },
+
+        contentContainer: {
+            paddingTop: 30,
+        },
+    });
+}
---
+++ screens/nav_bar/HomeScreen.js
@@ -0,0 +1,43 @@
+import * as WebBrowser from 'expo-web-browser';
+import React from 'react';
+import {
+  Image,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+
+import { MonoText } from '../../components/StyledText';
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <ScrollView
+        style={styles.container}
+        contentContainerStyle={styles.contentContainer}>
+
+
+      </ScrollView>
+    </View>
+  );
+}
+
+HomeScreen.navigationOptions = {
+  header: null,
+};
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+
+  contentContainer: {
+    paddingTop: 30,
+  },
+});
